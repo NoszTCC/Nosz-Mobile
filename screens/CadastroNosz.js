cc8e52376a220d8ecb5054a0b3786ec6be8eb3d9
KawanNasc <atlaskawan@gmail.com>
2024-03-12 21:55:41 -0300

    Otimização dos estilos

--- screens/CadastroNosz.js
+++ screens/CadastroNosz.js
@@ -1,42 +1,24 @@
 import React, { useState, useEffect } from 'react';
 import { Text, SafeAreaView, View, StyleSheet, Image, TouchableOpacity, TextInput, Alert } from 'react-native';
 import { useFonts, Montserrat_400Regular, Montserrat_500Medium, Montserrat_600SemiBold, Montserrat_700Bold, Montserrat_800ExtraBold, Montserrat_900Black } from '@expo-google-fonts/montserrat';
-import { getAuth, createUserWithEmailAndPassword } from '@firebase/auth';
+import { getAuth, createUserWithEmailAndPassword } from 'firebase/auth';
 import * as SplashScreen from 'expo-splash-screen';
 import { initializeApp } from '@firebase/app';
 import firebaseConfig from '../firebaseConfig';
+import { estilizar } from '../assets/EstilosGerais';
 
 const app = initializeApp(firebaseConfig);
 
 export default function Cadastro({ navigation }) {
+
+  // ESTILOS
+  const estilosGerais = estilizar();
+
   // PEGAR DADOS DO USUÁRIO
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [confirmPassword, setConfirmPassword] = useState('');
   const auth = getAuth(app);
-
-  // CONFIGURAÇÃO DAS FONTES
-  const [fontsLoaded] = useFonts({
-    Montserrat_400Regular,
-    Montserrat_500Medium,
-    Montserrat_600SemiBold,
-    Montserrat_700Bold,
-    Montserrat_800ExtraBold,
-    Montserrat_900Black,
-  });
-
-  useEffect(() => {
-    async function loadResourcesAndDataAsync() {
-      try {
-        SplashScreen.preventAutoHideAsync();
-      } catch (e) {
-        console.warn(e);
-      } finally {
-        await SplashScreen.hideAsync(); 
-      }
-    }
-    loadResourcesAndDataAsync();
-  }, []);
 
   // INICIO CÓDIGO DE CADASTRO
   const handleCadastro = async () => {
@@ -58,34 +40,30 @@ export default function Cadastro({ navigation }) {
     }
   };
 
-  if (!fontsLoaded) {
-    return null;
-  }
-
   return (
-    <SafeAreaView style={styles.container}>
-      <View style={styles.content}>
-        <View style={styles.vheader}>
+    <SafeAreaView style={estilosGerais.container}>
+      <View style={estilosGerais.content}>
+        <View style={estilosGerais.vheader}>
           <View style={styles.preheader}>
             <View style={styles.vgoback}>
               <TouchableOpacity
                 onPress={() => navigation.navigate('Pagina Inicial', { title: 'Início' })}
               >
-                <Image source={require('../assets/images/voltar-icon.png')} style={styles.voltar} />
+                <Image source={require('../assets/images/voltar-icon.png')} style={estilosGerais.voltar} />
               </TouchableOpacity>
             </View>
             <View style={styles.vtexto}>
-              <Text style={styles.t1}>Realize seu</Text>
-              <Text style={styles.t2}>Cadastro</Text>
-              <Text style={styles.txt}>Digite seu email e senha.</Text>
+              <Text style={estilosGerais.t1}>Realize seu</Text>
+              <Text style={estilosGerais.t2}>Cadastro</Text>
+              <Text style={estilosGerais.txt}>Digite seu email e senha.</Text>
             </View>
           </View>
-          <Image source={require('../assets/images/nosz.png')} style={styles.nosz} />
+          <Image source={require('../assets/images/nosz.png')} style={estilosGerais.nosz} />
           <View style={styles.circulo}></View>
         </View>
         <View style={styles.vinputs}>
           <TextInput
-            style={styles.input}
+            style={estilosGerais.input}
             placeholder='Email'
             cursorColor={'#E46216'}
             value={email}
@@ -93,7 +71,7 @@ export default function Cadastro({ navigation }) {
             autoCapitalize='none'
           />
           <TextInput
-            style={styles.input}
+            style={estilosGerais.input}
             placeholder='Senha'
             cursorColor={'#E46216'}
             value={password}
@@ -101,7 +79,7 @@ export default function Cadastro({ navigation }) {
             secureTextEntry
           />
           <TextInput
-            style={styles.input}
+            style={estilosGerais.input}
             placeholder='Confirmar Senha'
             cursorColor={'#E46216'}
             value={confirmPassword}
@@ -109,7 +87,7 @@ export default function Cadastro({ navigation }) {
             secureTextEntry
           />
         </View>
-        <View style={styles.buttonview}>
+        <View style={estilosGerais.buttonview}>
           <TouchableOpacity
             style={styles.buttonCadastro}
             onPress={handleCadastro}
@@ -125,48 +103,6 @@ export default function Cadastro({ navigation }) {
 
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  vheader: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-  },
-  voltar: {
-    width: 34,
-    height: 40,
-    margin: 10
-  },
-  nosz: {
-    width: 170,
-    height: 170,
-    alignSelf: 'flex-end',
-    zIndex: -1
-  },
-  t1: {
-    fontFamily: 'Montserrat_600SemiBold',
-    textAlign: 'left',
-    fontSize: 35,
-    fontWeight: 'bold',
-    color: '#000',
-    alignSelf: 'flex-start',
-  },
-  t2: {
-    fontFamily: 'Montserrat_700Bold',
-    textAlign: 'left',
-    fontSize: 35,
-    alignSelf: 'flex-start',
-    color: '#e36216',
-  },
-  txt: {
-    fontFamily: 'Montserrat_600SemiBold',
-    textAlign: 'left',
-    fontSize: 18,
-    color: '#565656',
-    alignSelf: 'flex-start',
-  },
   circulo: {
     position: 'absolute',
     backgroundColor: '#f89a14',
@@ -182,14 +118,6 @@ const styles = StyleSheet.create({
     justifyContent: 'flex-end',
     marginBottom: 80 
   },
-  input: {
-    borderBottomColor: '#000000',
-    borderBottomWidth: 1,
-    color: '#131313',
-    height: 35,
-    fontSize: 19,
-    margin: 20
-  },
   circulo2: {
     position: 'absolute',
     backgroundColor: '#f89a14',
@@ -199,21 +127,6 @@ const styles = StyleSheet.create({
     bottom: '-90%',
     right: '-4%',
     zIndex: -999,
-  },
-  content: {
-    flex: 1,
-    justifyContent: 'space-between',
-    backgroundColor: '#ecf0f1',
-    margin: 10,
-    padding: 18,
-    width: '100%',
-    height: '100%',
-  },
-  buttonview: {
-    flexDirection: 'column',
-    justifyContent: 'space-between',
-    justifyContent: 'center',
-    alignItems: 'center',
   },
   buttonCadastro: {
     backgroundColor: '#479d32',
--- screens/LoginNosz.js
+++ screens/LoginNosz.js
@@ -1,41 +1,19 @@
 import React, { useState, useEffect } from 'react';
 import { Text, SafeAreaView, View, StyleSheet, Image, TouchableOpacity, TextInput, Alert } from 'react-native';
 import { useFonts, Montserrat_400Regular, Montserrat_500Medium, Montserrat_600SemiBold, Montserrat_700Bold, Montserrat_800ExtraBold, Montserrat_900Black } from '@expo-google-fonts/montserrat';
-import * as SplashScreen from 'expo-splash-screen';
 import { initializeApp } from '@firebase/app';
-import { getAuth, signInWithEmailAndPassword } from '@firebase/auth';
+import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
 import firebaseConfig from '../firebaseConfig';
+import { estilizar } from '../assets/EstilosGerais';
 
 const app = initializeApp(firebaseConfig);
 
 export default function Login({ navigation }) {
+
+  const estilosGerais = estilizar();
+  
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-
-  let [fontsLoaded] = useFonts({
-    Montserrat_400Regular,
-    Montserrat_500Medium,
-    Montserrat_600SemiBold,
-    Montserrat_700Bold,
-    Montserrat_800ExtraBold,
-    Montserrat_900Black,
-  });
-
-  useEffect(() => {
-    async function loadResourcesAndDataAsync() {
-      try {
-        SplashScreen.preventAutoHideAsync();
-      } catch (e) {
-        console.warn(e);
-      } finally {
-        await SplashScreen.hideAsync();
-      }
-    }
-
-    if (!fontsLoaded) {
-      loadResourcesAndDataAsync();
-    }
-  }, [fontsLoaded]);
 
   const handleAuthentication = async () => {
     try {
@@ -48,32 +26,28 @@ export default function Login({ navigation }) {
     }
   };
 
-  if (!fontsLoaded) {
-    return null;
-  }
-
   return (
-    <SafeAreaView style={styles.container}>
-      <View style={styles.content}>
-        <View style={styles.vheader}>
+    <SafeAreaView style={estilosGerais.container}>
+      <View style={estilosGerais.content}>
+        <View style={estilosGerais.vheader}>
           <View style={styles.preheader}>
             <View style={styles.vgoback}>
               <TouchableOpacity onPress={() => navigation.navigate('Pagina Inicial', { title: 'Início', })}>
-                <Image source={require('../assets/images/voltar-icon.png')} style={styles.voltar} />
+                <Image source={require('../assets/images/voltar-icon.png')} style={estilosGerais.voltar} />
               </TouchableOpacity>
             </View>
             <View style={styles.vtexto}>
-              <Text style={styles.t1}>Realize seu</Text>
-              <Text style={styles.t2}>Login</Text>
-              <Text style={styles.txt}>Digite seu email e senha.</Text>
+              <Text style={estilosGerais.t1}>Realize seu</Text>
+              <Text style={estilosGerais.t2}>Login</Text>
+              <Text style={estilosGerais.txt}>Digite seu email e senha.</Text>
             </View>
           </View>
-          <Image source={require('../assets/images/nosz.png')} style={styles.nosz} />
+          <Image source={require('../assets/images/nosz.png')} style={estilosGerais.nosz} />
           <View style={styles.circulo}></View>
         </View>
         <View style={styles.vinputs}>
           <TextInput
-            style={styles.input}
+            style={estilosGerais.input}
             placeholder='Email'
             cursorColor={'#E46216'}
             value={email}
@@ -81,7 +55,7 @@ export default function Login({ navigation }) {
             autoCapitalize='none'
           />
           <TextInput
-            style={styles.input}
+            style={estilosGerais.input}
             placeholder='Senha'
             cursorColor={'#E46216'}
             value={password}
@@ -89,7 +63,7 @@ export default function Login({ navigation }) {
             secureTextEntry
           />
         </View>
-        <View style={styles.buttonview}>
+        <View style={estilosGerais.buttonview}>
           <TouchableOpacity style={styles.buttonLogin} onPress={handleAuthentication}>
             <Text style={styles.buttonTextLogin}>Fazer Login</Text>
           </TouchableOpacity>
@@ -108,48 +82,6 @@ export default function Login({ navigation }) {
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  vheader: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-  },
-  voltar: {
-    width: 34,
-    height: 40,
-    margin: 10
-  },
-  nosz: {
-    width: 170,
-    height: 170,
-    alignSelf: 'flex-end',
-    zIndex: -1
-  },
-  t1: {
-    fontFamily: 'Montserrat_600SemiBold',
-    textAlign: 'left',
-    fontSize: 35,
-    fontWeight: 'bold',
-    color: '#000',
-    alignSelf: 'flex-start',
-  },
-  t2: {
-    fontFamily: 'Montserrat_700Bold',
-    textAlign: 'left',
-    fontSize: 35,
-    alignSelf: 'flex-start',
-    color: '#FA9914',
-  },
-  txt: {
-    fontFamily: 'Montserrat_600SemiBold',
-    textAlign: 'left',
-    fontSize: 18,
-    color: '#565656',
-    alignSelf: 'flex-start',
-  },
   circulo: {
     position: 'absolute',
     backgroundColor: '#0EAA00',
@@ -164,14 +96,6 @@ const styles = StyleSheet.create({
     flex: 1, 
     justifyContent: 'flex-end', 
   },
-  input: {
-    borderBottomColor: '#000000',
-    borderBottomWidth: 1,
-    color: '#131313',
-    height: 35,
-    fontSize: 19,
-    margin: 20
-  },
   circulo2: {
     position: 'absolute',
     backgroundColor: '#0EAA00',
@@ -181,21 +105,6 @@ const styles = StyleSheet.create({
     bottom: '-90%',
     right: '-4%',
     zIndex: -999,
-  },
-  content: {
-    flex: 1,
-    justifyContent: 'space-between',
-    backgroundColor: '#ecf0f1',
-    margin: 10,
-    padding: 18,
-    width: '100%',
-    height: '100%',
-  },
-  buttonview: {
-    flexDirection: 'column',
-    justifyContent: 'space-between',
-    justifyContent: 'center',
-    alignItems: 'center',
   },
   buttonLogin: {
     backgroundColor: '#E46216',
